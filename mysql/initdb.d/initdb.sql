0211adb98cbe5831ba4ce51e32083a0254c1656d
cyfblog <50159489+github-cyf@users.noreply.github.com>
2019-10-22 16:01:22 +0800

    Update initdb.sql

--- mysql/initdb.d/initdb.sql
+++ mysql/initdb.d/initdb.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS onlyoffice CHARACTER SET "utf8" COLLATE "utf8_general_ci";
 CREATE DATABASE IF NOT EXISTS onlyoffice_mailserver CHARACTER SET "utf8" COLLATE "utf8_general_ci";
 
-CREATE USER IF NOT EXISTS 'community'@'%' IDENTIFIED BY '123456';
-CREATE USER IF NOT EXISTS 'mail'@'%' IDENTIFIED BY '123456';
+CREATE USER IF NOT EXISTS 'community'@'%' IDENTIFIED BY 'office@com123456';
+CREATE USER IF NOT EXISTS 'mail'@'%' IDENTIFIED BY 'office@mail123456';
 
 GRANT ALL PRIVILEGES ON onlyoffice.* TO 'community'@'%';
 GRANT ALL PRIVILEGES ON onlyoffice_mailserver.* TO 'community'@'%';
